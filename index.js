2e223f9e712ad7124976f3f50911ee732060688e
p l <pekka.laitinen@tuni.fi>
2020-02-09 14:45:45 +0200

    postgresql

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: 'postgres://wivmlryjtzfyfw:20f964ab6427825853443dc289a209208e7e5c37767969a99798e435449e9f72@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d82ibv4j551tjm',
+  ssl: true
+});
+
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
@@ -8,6 +14,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+//  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
